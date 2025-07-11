099f757a0448f24ca7bf5b82e786845dd49f2b24
Aryan Bhat <aryanbhat2527@gmail.com>
2025-07-11 19:10:55 +0530

    removed the generated output folder

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -1,4 +1,4 @@
-import express, { type Request, type Response } from 'express';
+import express from 'express';
 import dotenv from 'dotenv'
 import path from 'path'
 import { websiteRouter } from './routes/website.route';
--- apps/api/scripts/run-integration-tests.sh
+++ apps/api/scripts/run-integration-tests.sh
@@ -1,6 +1,6 @@
-docker-compose up -d
+docker compose up -d
 echo '🟡 - Waiting for database to be ready...'
 ./scripts/wait-for-it.sh "postgresql://postgres:mysecret@localhost:5432/postgres" -- echo '🟢 - Database is ready!'
 bunx prisma migrate dev --name init
 bun test
-docker-compose down
+docker compose down
